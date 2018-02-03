1dfe5996a02f833fad4e672ff13faf17a949e507
gameobjectw <gameobjectw@163.com>
2018-02-03 15:58:18 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int[,] map =
+        {
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            { 1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+            { 1,0,8,0,1,0,0,1,1,1,0,0,0,1,1},
+            { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,1},
+            { 1,0,0,0,1,1,1,1,1,1,1,1,1,0,1},
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            { 1,0,1,0,0,1,1,1,1,1,1,1,1,1,1},
+            { 1,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+            { 1,0,1,0,0,0,0,0,0,0,0,0,0,9,1},
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        };
+        public Point[,] mapPos;
+
+        //Queue<Node> AllPathPoint;
+        //Stack<Node> FinalPathPoint;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           // CreateLine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FindPathBFS bfs = new FindPathBFS();
+            FinalData fd= bfs.Start();
+            Draw(fd.AllPathPoint,fd.FinalPathPoint);
+        }
+        void CreateLine() {
+            mapPos = new Point[map.GetLength(0), map.GetLength(1)];
+
+            // 获取当前窗体的坐标
+            Rectangle rect = panel1.Bounds;
+            Graphics gra = panel1.CreateGraphics();
+            // 网格的列数
+            int col = map.GetLength(1) - 1;
+            // 网格的行数
+            int row = map.GetLength(0) - 1;
+            int colva = rect.Width / (col + 1);
+            int rowva = rect.Height / (row + 1);
+            int drawRow = 0;
+            int drawCol = 0;
+
+
+
+
+            // 实例化画笔。
+            Pen p = new Pen(Brushes.Blue);
+            // 设置画笔的样式(虚线)。
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+
+            // 画水平线
+            gra.DrawLine(p, 0, 0, rect.Width, 0);
+            gra.DrawLine(p, 0, rect.Height - 1, rect.Width, rect.Height - 1);
+            gra.DrawLine(p, 0, 0, 0, rect.Height);
+            gra.DrawLine(p, rect.Width - 1, 0, rect.Width - 1, rect.Height);
+
+            for (int i = 1; i <= row; i++)
+            {
+                drawRow += rowva;
+                gra.DrawLine(p, 0, drawRow, rect.Width, drawRow);
+                //  Console.WriteLine(rowva);
+            }
+            for (int j = 1; j <= col; j++)
+            {
+                drawCol += colva;
+                gra.DrawLine(p, drawCol, 0, drawCol, rect.Height);
+            }
+            SolidBrush sb = new SolidBrush(Color.Red);
+            int x = 0;
+            int y = 0;
+            int xx = 0;
+            int yy = 0;
+            foreach (int item in map)
+            {
+                xx = x % (col + 1);
+                yy = y / (col + 1);
+                if (item == 1)
+                {
+                    sb.Color = Color.Red;
+                }
+                if (item == 0)
+                {
+                    sb.Color = Color.White;
+                }
+                if (item == 8)
+                {
+                    sb.Color = Color.Pink;
+                }
+                if (item == 9)
+                {
+                    sb.Color = Color.Yellow;
+                }
+                gra.FillRectangle(sb, new Rectangle(new Point(colva * xx, rowva * yy), new Size(colva - 1, rowva - 1)));
+                mapPos[yy, xx] = new Point(colva * xx + colva - 1, rowva * yy + rowva - 1);
+                x++;
+                y++;
+            }
+
+        }
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+            mapPos = new Point[map.GetLength(0), map.GetLength(1)];
+
+            // 获取当前窗体的坐标
+            Rectangle rect = panel1.Bounds;
+            // 网格的列数
+            int col = map.GetLength(1) - 1;
+            // 网格的行数
+            int row = map.GetLength(0) - 1;
+            int colva = rect.Width / (col + 1);
+            int rowva = rect.Height / (row + 1);
+            int drawRow = 0;
+            int drawCol = 0;
+
+
+
+
+            // 实例化画笔。
+            Pen p = new Pen(Brushes.Blue);
+            // 设置画笔的样式(虚线)。
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+
+            // 画水平线
+            e.Graphics.DrawLine(p, 0, 0, rect.Width, 0);
+            e.Graphics.DrawLine(p, 0, rect.Height - 1, rect.Width, rect.Height - 1);
+            e.Graphics.DrawLine(p, 0, 0, 0, rect.Height);
+            e.Graphics.DrawLine(p, rect.Width - 1, 0, rect.Width - 1, rect.Height);
+
+            for (int i = 1; i <= row; i++)
+            {
+                drawRow += rowva;
+                e.Graphics.DrawLine(p, 0, drawRow, rect.Width, drawRow);
+                //  Console.WriteLine(rowva);
+            }
+            for (int j = 1; j <= col; j++)
+            {
+                drawCol += colva;
+                e.Graphics.DrawLine(p, drawCol, 0, drawCol, rect.Height);
+            }
+            SolidBrush sb = new SolidBrush(Color.Red);
+            int x = 0;
+            int y = 0;
+            int xx = 0;
+            int yy = 0;
+            foreach (int item in map)
+            {
+                xx = x % (col + 1);
+                yy = y / (col + 1);
+                if (item == 1)
+                {
+                    sb.Color = Color.Red;
+                }
+                if (item == 0)
+                {
+                    sb.Color = Color.White;
+                }
+                if (item == 8)
+                {
+                    sb.Color = Color.Pink;
+                }
+                if (item == 9)
+                {
+                    sb.Color = Color.Yellow;
+                }
+                e.Graphics.FillRectangle(sb, new Rectangle(new Point(colva * xx, rowva * yy), new Size(colva - 1, rowva - 1)));
+                mapPos[yy, xx] = new Point(colva * xx+(colva - 1) /2 , rowva * yy+(rowva - 1) /2 );
+                x++;
+                y++;
+            }
+        }
+        public void Draw(Queue<Node> AllPathPoint, Stack<Node> FinalPathPoint)
+        {
+            panel2.Refresh();
+            Pen p = new Pen(Brushes.Black);
+            
+            // 设置画笔的样式(虚线)。
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            while (AllPathPoint.Count != 0)
+            {
+                Node node = AllPathPoint.Dequeue();
+                Point ParentPos = mapPos[node.Parent.SelfIndex.X, node.Parent.SelfIndex.Y];
+                Point SelfPos = mapPos[node.SelfIndex.X, node.SelfIndex.Y];
+                panel2.CreateGraphics().DrawLine(p, ParentPos, SelfPos);
+                Thread.Sleep(100);
+            }
+            p = new Pen(Brushes.Green);
+            // 设置画笔的样式(虚线)。
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            p.Width = 3f;
+            while (FinalPathPoint.Count!=0)
+            {
+                Node node = FinalPathPoint.Pop();
+                Point ParentPos = mapPos[node.Parent.SelfIndex.X, node.Parent.SelfIndex.Y];
+                Point SelfPos = mapPos[node.SelfIndex.X, node.SelfIndex.Y];
+                panel2.CreateGraphics().DrawLine(p, ParentPos, SelfPos);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FindPathBFS bfs = new FindPathBFS();
+            FinalData fd = bfs.BFSStart();
+            Draw(fd.AllPathPoint, fd.FinalPathPoint);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            float a = 4.6f;
+            a += 0.5f;
+            Console.WriteLine((int)a);
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            
+            
+        }
+    }
+    class FinalData {
+        public Queue<Node> AllPathPoint;
+        public Stack<Node> FinalPathPoint;
+
+        public FinalData(Queue<Node> allPathPoint, Stack<Node> finalPathPoint)
+        {
+            AllPathPoint = allPathPoint;
+            FinalPathPoint = finalPathPoint;
+        }
+    }
+    class FindPathBFS {
+        public int[,] map = 
+        {
+           { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            { 1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+            { 1,0,8,0,1,0,0,1,1,1,0,0,0,1,1},
+            { 1,0,0,0,1,0,0,0,0,1,0,0,0,0,1},
+            { 1,0,0,0,1,1,1,1,1,1,1,1,1,0,1},
+            { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            { 1,0,1,0,0,1,1,1,1,1,1,1,1,1,1},
+            { 1,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+            { 1,0,1,0,0,0,0,0,0,0,0,0,0,9,1},
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        };
+
+        Stack<Node> PathPointStack=new Stack<Node>();
+
+
+        Queue<Node> PathPoint = new Queue<Node>();
+        public Queue<Node> AllPathPoint = new Queue<Node>();
+        List<Point> BeCheckedPoint;
+        public Stack<Node> FinalPathPoint = new Stack<Node>();
+        bool isFinish=false;
+        public FinalData Start() {
+            BeCheckedPoint = new List<Point>();
+            Node StartPoint = new Node()
+            {
+                SelfIndex = new Point(2, 2),
+                Parent = null
+            };
+            PathPointStack.Push(StartPoint);
+        //    AllPathPoint.Enqueue(StartPoint);
+            while (PathPointStack.Count!=0&&!isFinish)
+            {
+                DFSCheckBoundPoint8(PathPointStack.Pop(), 15, 10);
+            }
+            return new FinalData(AllPathPoint, FinalPathPoint);
+        }
+        public FinalData BFSStart()
+        {
+            BeCheckedPoint = new List<Point>();
+            Node StartPoint = new Node()
+            {
+                SelfIndex = new Point(2, 2),
+                Parent = null
+            };
+            PathPoint.Enqueue(StartPoint);
+            //    AllPathPoint.Enqueue(StartPoint);
+            while (PathPoint.Count != 0 && !isFinish)
+            {
+                BFSCheckBoundPoint8(PathPoint.Dequeue(), 15, 10);
+            }
+            return new FinalData(AllPathPoint, FinalPathPoint);
+        }
+        private void DFSCheckBoundPoint8(Node node,int width,int height) {
+            Point index = node.SelfIndex;
+            
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <=1; j++)
+                {
+                    if (i!=0||j!=0) {
+                        
+                        if ((index.X+i)<0|| (index.Y + j)<0|| (index.X + i)>height|| (index.Y + j)>width|| CheckPointIsChecked(new Point(index.X + i, index.Y + j))) {
+                            continue;
+                        }
+                        Point point = new Point(index.X + i, index.Y + j);
+                        if (CheckPointIsFinal(point)) {
+                            isFinish = true;
+                            Node ParentNode = node;
+                            Node FinalNode = new Node
+                            {
+                                SelfIndex = new Point(index.X + i, index.Y + j),
+                                Parent = node
+                            };
+                            AllPathPoint.Enqueue(FinalNode);
+                            FinalPathPoint.Push(FinalNode);
+                            int x = 0;
+                            while (ParentNode.Parent!=null)
+                            {
+                                x++;
+                                Console.WriteLine("父路径点     " + ParentNode.Parent.SelfIndex);
+                                FinalPathPoint.Push(ParentNode);
+                                ParentNode = ParentNode.Parent;
+                            }
+                         //   new Form1().Draw(AllPathPoint,FinalPathPoint);
+                            Console.WriteLine("路径总长度：     " + x);
+                        }
+                        if (CheckPointIsCanWalk(point)) {
+                            Node thisNode = new Node
+                            {
+                                SelfIndex = point,
+                                Parent = node
+                            };
+                            PathPointStack.Push(thisNode);
+                            AllPathPoint.Enqueue(thisNode);
+                            BeCheckedPoint.Add(point);
+                            return;
+                        }
+                    }
+                }
+            }
+            PathPointStack.Push(node.Parent);
+        }
+        private void BFSCheckBoundPoint8(Node node, int width, int height)
+        {
+            Point index = node.SelfIndex;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+
+                        if ((index.X + i) < 0 || (index.Y + j) < 0 || (index.X + i) > height || (index.Y + j) > width || CheckPointIsChecked(new Point(index.X + i, index.Y + j)))
+                        {
+                            continue;
+                        }
+                        Point point = new Point(index.X + i, index.Y + j);
+                        if (CheckPointIsFinal(point))
+                        {
+                            isFinish = true;
+                            Node ParentNode = node;
+                            Node FinalNode = new Node
+                            {
+                                SelfIndex = new Point(index.X + i, index.Y + j),
+                                Parent = node
+                            };
+                            AllPathPoint.Enqueue(FinalNode);
+                            FinalPathPoint.Push(FinalNode);
+                            int x = 0;
+                            while (ParentNode.Parent != null)
+                            {
+                                x++;
+                                Console.WriteLine("父路径点     " + ParentNode.Parent.SelfIndex);
+                                FinalPathPoint.Push(ParentNode);
+                                ParentNode = ParentNode.Parent;
+                            }
+                            //   new Form1().Draw(AllPathPoint,FinalPathPoint);
+                            Console.WriteLine("路径总长度：     " + x);
+                        }
+                        if (CheckPointIsCanWalk(point))
+                        {
+                            Node thisNode = new Node
+                            {
+                                SelfIndex = point,
+                                Parent = node
+                            };
+                            PathPoint.Enqueue(thisNode);
+                            AllPathPoint.Enqueue(thisNode);
+                            BeCheckedPoint.Add(point);
+                        }
+                    }
+                }
+            }
+        }
+        private bool CheckPointIsCanWalk(Point point) {
+            return map[point.X, point.Y] == 0;
+        }
+        private bool CheckPointIsFinal(Point point) {
+            return map[point.X, point.Y] == 9;
+        }
+        private bool CheckPointIsChecked(Point point) {
+            return BeCheckedPoint.Contains(point);
+        }
+    }
+    public class Node {
+        public Point SelfIndex;
+        public Node Parent; 
+    }
+}
